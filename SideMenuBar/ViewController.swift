8d64449decbbca4f591ffcc0b69be521bd296445
Rakesh Kumar <rakeshkumar@rakeshs-imac.local>
2019-03-26 12:47:54 +0530

    hihjhkcfgj

--- SideMenuBar/ViewController.swift
+++ SideMenuBar/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var sideMenuOpen = false
     override func viewDidLoad() {
         super.viewDidLoad()
+        //hi sakshi
         NotificationCenter.default.addObserver(self, selector: #selector(toggleSideMenu), name: NSNotification.Name("Toggle"), object: nil)
       
         //add swipe gestures
